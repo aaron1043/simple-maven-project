df5508eaa75c80202ba60aca310f5d61f2eca4e2
Aaron Peña <l.aarongalvan@gmail.com>
2025-06-03 19:35:42 -0500

    Agregamos pruebas

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,4 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @Test
+    void sum() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    void multiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.multiply(2, 2));
+    }
+
+    @Test
+    void subtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(4, 2));
+
+    }
 }
